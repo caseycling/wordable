1b80e48cad90089089f2baeed7fb1005793ec3ad
Casey Clingaman <caseyclinga@gmail.com>
2024-11-09 12:41:11 -0800

    Implemented random word generation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+
 import './App.css';
 
 function App() {
+
+  const [dailyWord, setDailyWord] = useState('');
+
+  useEffect(() => {
+    fetch("https://random-word-api.herokuapp.com/word?length=5")
+    .then(res => res.json())
+    .then(randoWord => setDailyWord(randoWord[0]))
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {dailyWord}    
     </div>
   );
 }
